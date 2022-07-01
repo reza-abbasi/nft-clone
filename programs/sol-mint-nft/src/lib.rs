7969cd5e5521d2407d0dd30cafcb35553a182cbe
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-01 17:49:04 +0430

    change program_id

--- programs/sol-mint-nft/src/lib.rs
+++ programs/sol-mint-nft/src/lib.rs
@@ -4,7 +4,7 @@ use anchor_spl::token::mint_to;
 use anchor_spl::token::{MintTo, Token};
 use mpl_token_metadata::instruction::{create_metadata_accounts_v2};
 
-declare_id!("9FKLho9AUYScrrKgJbG1mExt5nSgEfk1CNEbR8qBwKTZ");
+declare_id!("8fgUWBN1YFEKwK6kRTTMWj16ho9VNWLW12mPyxQPMLx1");
 
 #[program]
 pub mod sol_mint_nft {
